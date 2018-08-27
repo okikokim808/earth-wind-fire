57aa89bf59c837258473ab66bcf0f4e0b85a539f
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-27 13:50:53 -0700

    edited seed.js

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 
+const express = require('express');
+const app = express();
+
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+
+const db = require('./models');
+
+
+app.use(express.static('public'));
+
+
+
+
+app.get('/', function homepage(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
 
 
 
 app.get('/api/hubs', (req, res) => {
-    db.User.find({}, (err, user) => {
+    db.User.find({ }, (err, user) => {
         if(err) {
             return console.log(err)
         }
 
         res.json(user)
-    })
+    
 })
+
+});
 
 
 
